f522c7ac437d2dc65ad315ffe99d683311aa8766
Marco <marco.odore@agilelab.it>
2024-02-19 14:40:08 +0100

    first clustering

--- POC_conversations/main.py
+++ POC_conversations/main.py
@@ -14,9 +14,10 @@ from IPython.display import display
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    df = pd.read_csv("https://renumics.com/data/mnist/mnist-tiny.csv")
+    df = pd.read_csv("test.csv")
     display(df)
-    spotlight.show(df, dtype={"image": spotlight.Image})
+    df = df.drop('Unnamed: 0.1', axis=1).drop('Unnamed: 0', axis=1)
+    spotlight.show(df)
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## Dataset
 
 https://www.kaggle.com/datasets/aimack/customer-service-chat-data-30k-rows?resource=download
+
+## Reference for Sentence Clustering
+
+https://towardsdatascience.com/clustering-sentence-embeddings-to-identify-intents-in-short-text-48d22d3bf02e
