7b9b47e389c26360622559c87b34867c1c8afac1
Marco <marco.odore@agilelab.it>
2024-02-19 17:00:54 +0100

    added automatic labeling

--- POC_conversations/main.py
+++ POC_conversations/main.py
@@ -14,9 +14,9 @@ from IPython.display import display
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    df = pd.read_csv("test.csv")
+    df = pd.read_csv("test2.csv")
     display(df)
-    df = df.drop('Unnamed: 0.1', axis=1).drop('Unnamed: 0', axis=1)
+    df = df
     spotlight.show(df)
 
 
