1d11d899db7db36fae0c8048ec34a2e89d3be64a
Marco <marco.odore@agilelab.it>
2024-02-21 11:21:58 +0100

    updated new dataset

--- POC_conversations/main.py
+++ POC_conversations/main.py
@@ -9,12 +9,9 @@ from renumics import spotlight
 from IPython.display import display
 
 
-
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    df = pd.read_csv("test2.csv")
+    df = pd.read_csv("banking.csv")
     display(df)
     df = df
     spotlight.show(df)
