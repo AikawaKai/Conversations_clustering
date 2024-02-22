41c3963c50394d3493ac64ba8c0fcc7b18e79ae3
RiccardoFino <riccardo.s.fino@gmail.com>
2024-02-22 15:12:38 +0100

    added LLM representation to bertopic

--- POC_conversations/main.py
+++ POC_conversations/main.py
@@ -14,7 +14,9 @@ from IPython.display import display
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    df = pd.read_pickle("embedding_L12_clustered.pkl")
+    df = pd.read_pickle("embedding_L12_clustered_LLM.pkl")
+    display(df)
+    print(df["Zephyr"])
     spotlight.show(df)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
