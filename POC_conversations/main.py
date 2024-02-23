b3bbd9197dda2c7cfd55bc537734a5ef7c92cbb9
Marco <marco.odore@agilelab.it>
2024-02-23 15:55:54 +0100

    added bertopic banking

--- POC_conversations/main.py
+++ POC_conversations/main.py
@@ -8,13 +8,10 @@ from renumics import spotlight
 from IPython.display import display
 
 
-
-
-
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    df = pd.read_pickle("embedding_L12_clustered_LLM.pkl")
+    df = pd.read_pickle("embedding_L12_clustered_LLM_banking_classes.pkl")
     display(df)
     print(df["Zephyr"])
     spotlight.show(df)
